c73929a3d2a5dec30b463cafcfcb98a98ba23daa
Schmelzofen <sertac.oezdemir@gmail.com>
2021-10-25 20:22:17 +0200

    js intro cc

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var helloWorld = "Hello World"
+const pi = 3.14
+const burjDubai = "828m"
+let fullName = "Jan Schmidt"
+const eifelTower = "324m"
+var camelCase = "camelCase"
+var kursStatus = "true"
+
+console.log(kursStatus)
